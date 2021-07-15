b104f2aca8bdb681b4dfe4c694ea125a65bb52d7
silenttowergames <silenttowergames@gmail.com>
2021-07-15 14:27:28 -0400

    Added better MenuItem shortcut macros

--- src/STE/ECS/Components/MenuItemFunctions.h
+++ src/STE/ECS/Components/MenuItemFunctions.h
@@ -6,9 +6,35 @@ void MenuItem_Select_Test(ApplicationState* app, ecs_entity_t entityID, Menu* me
 
 void MenuItem_Select_Disable(ApplicationState* app, ecs_entity_t entityID, Menu* menu);
 
+#define menuItemSetup(menuEntityID, itemFontName, itemFontSize, itemVarName) \
+    ecs_entity_t meid = menuEntityID;\
+    ecs_entity_t* mitems = itemVarName;\
+    const char* miFontName = itemFontName;\
+    int miFontSize = itemFontSize;
+
 #define menuItemEasy(menuEntityID, itemVarName, itemCountVarName, renderableValue, itemSelectEvent, itemHoveringEvent) \
     itemVarName[itemCountVarName] = ecs_new(world, 0); \
     ecs_set(world, itemVarName[itemCountVarName], Body, bodyEasy(0, 0)); \
     ecs_set(world, itemVarName[itemCountVarName], Renderable, renderableValue); \
     ecs_set(world, itemVarName[itemCountVarName], MenuItem, { menuEntityID, itemSelectEvent, itemHoveringEvent, }); \
     itemCountVarName++;
+
+#define menuItemSuperEasy(renderableText, itemSelectEvent, itemHoveringEvent) \
+    menuItemEasy(\
+        meid,\
+        mitems,\
+        currentItem,\
+        RenderableText(\
+            miFontName,\
+            miFontSize,\
+            layer,\
+            1,\
+            0,\
+            colorU(255, 255, 255, 255),\
+            0,\
+            renderableText,\
+            false\
+        ),\
+        itemSelectEvent,\
+        itemHoveringEvent\
+    )
