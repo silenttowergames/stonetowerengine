2a944f63cef2f9274a3858c9241271e8aec7c80c
silenttowergames <silenttowergames@gmail.com>
2021-07-21 19:19:00 -0400

    Draw sort fixed :)

--- src/STE/ECS/Systems/DrawSystem.h
+++ src/STE/ECS/Systems/DrawSystem.h
@@ -7,5 +7,5 @@ void DrawSystem(ecs_iter_t* it);
 #define DrawSystem_Init() \
     ECS_SYSTEM(world, DrawSystem, EcsOnUpdate, Body, Renderable);\
     ECS_SYSTEM(world, FinalizeScreenSystem, EcsOnUpdate, 0);\
-    ecs_query_t* sort = ecs_query_init(world, &(ecs_query_desc_t){ .filter.terms = { { ecs_id(Renderable), }, }, });\
+    ecs_query_t* sort = ecs_get_system_query(world, DrawSystem);\
     ecs_query_order_by(world, sort, ecs_typeid(Renderable), SortByLayerThenY);
